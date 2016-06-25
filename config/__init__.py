a0da43a3efaa4fe9e8e9fcea8482c786e7ac0bcb
palmerjoshua <palmerjoshua2013@fau.edu>
2016-06-24 20:47:38 -0400

    create log directory and file if it doesn't exist

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,18 @@
 from logging import getLogger
 from logging.config import fileConfig
 import os
-directory = os.path.dirname(__file__)
-fileConfig(os.path.join(directory, "log_config.ini"))
 
+config_directory = os.path.dirname(__file__)
+root = os.path.dirname(config_directory)
+log_directory = os.path.join(root, 'logs')
+log_file_name = os.path.join(log_directory, "botlog.log")
+
+if not os.path.exists(log_directory):
+    os.mkdir(log_directory)
+
+if not os.path.exists(log_file_name):
+    with open(log_file_name, "a"):
+        pass
+
+
+fileConfig(os.path.join(config_directory, "log_config.ini"))
