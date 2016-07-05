be0a9525b5f17cdef8d99e715f31c27ca112286a
palmerjoshua <palmerjoshua2013@fau.edu>
2016-07-05 12:14:03 -0400

    config supports multiple subreddit destinations
    added full path to fix error

--- config/bot_config.py
+++ config/bot_config.py
@@ -1,4 +1,7 @@
 import yaml
+import os
+from config import config_directory
 
-with open("bot_config.yaml", "r") as ifile:
+bot_config_path = os.path.join(config_directory, "bot_config.yaml")
+with open(bot_config_path, "r") as ifile:
     CONFIG = yaml.load(ifile)
