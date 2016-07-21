12319a59a14138a1c888c12628152c6445129e7d
palmerjoshua <palmerjoshua2013@fau.edu>
2016-07-21 13:17:39 -0400

    script to generate reddit table containing event info

--- eventbot.py
+++ eventbot.py
@@ -1,28 +1,79 @@
+from config import getLogger
 from bs4 import BeautifulSoup
 import requests
 import json
 
-# Scrape the information from the upressonline
-r = requests.get("http://www.upressonline.com/fauevents/")
-data = r.text
-soup = BeautifulSoup(data, "html.parser")
-print(soup.prettify())
+BASE_URL = "http://www.upressonline.com/fauevents/"
+TABLE_ROW = "{title} | {date} | {description}\n"
+HEADER_DIVIDER = "---|---|----\n"
+TABLE_HEADER = TABLE_ROW.format(title='Title', date='Date', description='Description') + HEADER_DIVIDER
+logger = getLogger()
 
-data = []
-titles = []  # 3
-dates = []  # 6
-descriptions = []  # 8
 
-for event in soup.find_all('div', attrs={'data-tribejson': True}):
-    event_json = event.get('data-tribejson')
+def _get_event_html():
+    """
+    Makes the HTTP request to the event calendar website.
+    :return: String containing HTML, or None if the response is not 200 OK.
+    """
+    logger.info("Getting event calendar HTML from {}".format(BASE_URL))
+    r = requests.get(BASE_URL)
+    if r.status_code == requests.codes.ok:
+        data = r.text
+        return data
+    logger.warning("Returning None, Response not OK: code={}".format(r.status_code))
+    return None
+
+
+def _get_event_dict(event_json):
+    """
+    Takes the relevant values out of the event JSON and creates a simpler dictionary
+    that is used to format the string TABLE_ROW.
+    :param event_json: JSON stripped from the event's data-tribejson HTML attribute.
+    :type event_json: str
+    :return: A dict containing the relevant event data
+    """
     event_dict = json.loads(event_json)
-    titles.append(event_dict['title'])
-    dates.append(event_dict['dateDisplay'])
-    descriptions.append(event_dict['excerpt'][3:-4])
-    # the excerpt is still in a <p> tag, so I use string indexing to get only the text inside the tags.
+    return {'title': event_dict['title'],
+            'date': event_dict['dateDisplay'],
+            'description': event_dict['excerpt'][3:-4] or "None provided"}
 
-fmt = '{:<8}{:<25}{:<75}{}'
 
-print(fmt.format('', 'Title', 'Date', 'Description'))
-for i, (title, date, descrip) in enumerate(zip(titles, dates, descriptions)):
-    print(fmt.format(i, title, date, descrip))
+def _make_reddit_table(html):
+    """
+    Scrapes event data from HTML and creates a Reddit table with it.
+    :param html: HTML from the event website
+    :type data: str
+    :return: A single string containing a Reddit markdown table
+    """
+    logger.info("Generating reddit table")
+
+    # start with the header, and append a new row for each event
+    table = TABLE_HEADER
+    soup = BeautifulSoup(html, "html.parser")
+    for event in soup.find_all('div', attrs={'data-tribejson': True}):
+        event_json = event.get('data-tribejson')
+        event_dict = _get_event_dict(event_json)
+        table += TABLE_ROW.format(**event_dict)
+    return table
+
+
+def get_reddit_table():
+    """
+    Uses all the helper functions to get the HTML, scrape it, and generate a Reddit table.
+    :return: A single string containing a Reddit markdown table, or None if an error happens.
+    """
+    table = None
+    html = _get_event_html()
+    if html:
+        table = _make_reddit_table(html)
+    else:
+        logger.error("Table could not be generated.")
+    return table
+
+
+def main():
+    table = get_reddit_table()
+    print(table)
+
+if __name__ == '__main__':
+    main()
