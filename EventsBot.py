d6d99f48211f920b367194ccccae7ec9bfb028f0
palmerjoshua <palmerjoshua2013@fau.edu>
2016-07-21 11:26:03 -0400

    uncommented a line; fixed indentation

--- EventsBot.py
+++ EventsBot.py
@@ -6,7 +6,7 @@ import json
 r = requests.get("http://www.upressonline.com/fauevents/")
 data = r.text
 soup = BeautifulSoup(data, "html.parser")
-#print(soup.prettify())
+print(soup.prettify())
 
 data = []
 titles = []  # 3
@@ -14,12 +14,12 @@ dates = []  # 6
 descriptions = []  # 8
 
 for event in soup.find_all('div', attrs={'data-tribejson': True}):
-        event_json = event.get('data-tribejson')
-        event_dict = json.loads(event_json)
-        titles.append(event_dict['title'])
-        dates.append(event_dict['dateDisplay'])
-        descriptions.append(event_dict['excerpt'][3:-4])
-        # the excerpt is still in a <p> tag, so I use string indexing to get only the text inside the tags.
+    event_json = event.get('data-tribejson')
+    event_dict = json.loads(event_json)
+    titles.append(event_dict['title'])
+    dates.append(event_dict['dateDisplay'])
+    descriptions.append(event_dict['excerpt'][3:-4])
+    # the excerpt is still in a <p> tag, so I use string indexing to get only the text inside the tags.
 
 fmt = '{:<8}{:<25}{:<75}{}'
 
