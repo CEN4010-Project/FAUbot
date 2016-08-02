5b54133086f82082d965388636872b1ea9609244
palmerjoshua <palmerjoshua2013@fau.edu>
2016-08-02 19:08:01 -0400

    fixed bug; feature works now

--- eventbot.py
+++ eventbot.py
@@ -8,37 +8,24 @@ import json
 from pytz import timezone, utc
 from dateutil.parser import parse
 from bots import RedditBot
-from config.bot_config import CONFIG
-
+from config.bot_config import get_subreddits
 
 # region constants
-SUBMISSION_INTERVAL_HOURS = CONFIG['intervals']['submission_interval_hours']
-SUBREDDITS = CONFIG['subreddits']
-# endregion
-
-
 BASE_URL = "http://www.upressonline.com/fauevents/"
 TABLE_ROW = "{title} | {date} | {description}\n"
 HYPERLINK = "[{text}]({url})"
 HEADER_DIVIDER = "---|---|----\n"
 TABLE_HEADER = TABLE_ROW.format(title='Title', date='Date', description='Description') + HEADER_DIVIDER
+# endregion
+
 logger = getLogger()
-
-
-"""
-To use string formatting on the HYPERLINK template, you have to use keyword arguments, e.g.
-
-link = HYPERLINK.format(text="Click Me", url="http://example.com")
-
-This will let you put links inside your Reddit posts.
-"""
 
 
 class EventBot(RedditBot):
     def __init__(self, user_name, *args, **kwargs):
         super(EventBot, self).__init__(user_name=user_name, *args, **kwargs)
         self.base_url = BASE_URL
-        self.subreddits = CONFIG.get('subreddits', None) or ['FAUbot']
+        self.subreddits = get_subreddits()
         self.post_title = "Event Calendar"
 
     @staticmethod
@@ -58,7 +45,7 @@ class EventBot(RedditBot):
         now = utc.localize(datetime.datetime.utcnow())  # get current time in UTC timezone
         return now > start_datetime  # True if now is after start time
 
-    def get_title(event_json):
+    def get_title(self, event_json):
         event_dict = EventBot._get_event_dict(event_json)
         title = event_dict['title']
         return title
@@ -112,7 +99,7 @@ class EventBot(RedditBot):
                 table += TABLE_ROW.format(**event_dict)
         return table
 
-    def get_reddit_table(self):
+    def create_new_table(self):
         """
         Uses all the helper functions to get the HTML, scrape it, and generate a Reddit table.
         :return: A single string containing a Reddit markdown table, or None if an error happens.
@@ -125,61 +112,43 @@ class EventBot(RedditBot):
             logger.error("Table could not be generated.")
         return table
 
-
-    def get_existing_table_post(self,title, subreddit):
+    def get_existing_table_post(self, subreddit):
         """
-         Checks if a URL has already been shared on self.subreddit.
-         Because praw.Reddit.search returns a generator instead of a list,
-         we have to actually loop through it to see if the post exists.
-         If no post exists, the loop won't happen and it will return False.
-         :param post: The url that will be searched for
+         Searches a subreddit for a specific post. If found, return it. Else, return None.
          :param subreddit: The subreddit where the url will be searched for
-         :return: True if the url has already been posted to the subreddit
+         :return: a Reddit post object, or None
          """
-        for post in self.r.search("title:{} AND author:{}".format(title, self.USER_NAME), subreddit=subreddit):
+        for post in self.r.search("title:{} AND author:{}".format(self.post_title, self.USER_NAME), subreddit=subreddit):
             if post:
                 return post
         return None
 
-    def create_new_table(self):
-        html = self._get_event_html()
-        table = self._make_reddit_table(html)
-        return table
-
     def submit_new_table(self, table):
         """
-        Submit a link to Reddit, and save the submission time to the database.
-        :param title_tuple: A namedtuple with a url and a title.
+        Submit a new self post to Reddit containing a markdown table..
+        :param table: A string containing a reddit markdown table
         """
-        title = self.post_title
         for subreddit in self.subreddits:
-            self.r.submit(subreddit, title, text = table)
+            self.r.submit(subreddit, self.post_title, text=table)
             print(table)
 
-
     def work(self):
-        html = self._get_event_html()
-        soup = BeautifulSoup(html, "html.parser")
-        for event in soup.find_all('div', attrs={'data-tribejson': True}):
-            event_json = event.get('data-tribejson')
-        title = self.get_title(event_json)
         table = self.create_new_table()
         for subreddit in self.subreddits:
-            existing_post = self.get_existing_table_post(title, subreddit)
+            existing_post = self.get_existing_table_post(subreddit)
             if existing_post:  # if it exists
-                logger.info("Table already submitted")
+                logger.info("Editing existing table post")
                 existing_post.edit(table)
             else:
-                logger.info("Submitting Table")
-                self.submit_new_table(table)  # I added the word 'new' to be more specific
+                logger.info("Submitting new table post")
+                self.submit_new_table(table)
 
 
 def main():
     test = EventBot('FAUbot', run_once=True)  # the bot will only do one loop if you set that to True
     test.start()
-    test.stop_event.wait() #this keeps the program alive while the bot does its work in a separate
+    test.stop_event.wait()  # this keeps the program alive while the bot does its work in a separate thread
 
 
 if __name__ == '__main__':
     main()
-
